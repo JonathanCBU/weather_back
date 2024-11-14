fe3dd3c8f2d8d7f723f38ba7e474bf159eba0d88
Jonathan Cook <jcookbme@gmail.com>
2024-11-14 17:01:35 -0500

    add coords endpoint

--- weather_back/__server__.py
+++ weather_back/__server__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import dotenv
 from typing import Dict, Union, Any
@@ -41,12 +41,26 @@ def get_coordinates(loc: str) -> Dict[str, Union[str, int]]:
     else:
         # TODO: add logic to handle multiple locations returned from direct
         location_resp = requests.get(
-            f"http://api.openweathermap.org/geo/1.0/direct?q={loc}&appid={os.environ['OW_KEY']}"
+            f"http://api.openweathermap.org/geo/1.0/direct?q={loc}&appid={os.environ['OW_KEY']}",
+            timeout=5,
         )
         loc_json = location_resp.json()[0]
     # TODO: add error handling for GEO call here
 
     return loc_json
+
+
+def get_reverse_geoloc(lat: float, lon: float) -> Dict[str, Any]:
+    """get location info from coordinates
+
+    :param lat: latitude
+    :param lon: longitude
+    """
+    location_resp = requests.get(
+        f"http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit=1&appid={os.environ['OW_KEY']}",
+        timeout=5,
+    )
+    return location_resp.json()[0]
 
 
 def get_forecast(lat: float, lon: float) -> Dict[str, Any]:
@@ -56,7 +70,8 @@ def get_forecast(lat: float, lon: float) -> Dict[str, Any]:
     :param lon: longitude
     """
     forecast = requests.get(
-        f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&units=metric&exclude=minutely,hourly,alerts&appid={os.environ['OW_KEY']}"
+        f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&units=metric&exclude=minutely,hourly,alerts&appid={os.environ['OW_KEY']}",
+        timeout=5,
     )
     return forecast.json()
 
@@ -72,7 +87,7 @@ def main() -> None:
     _ = CORS(server, origins="*")
 
     @server.route("/weather/<string:loc>")
-    def get_weather(loc: str) -> Dict[str, Union[str, int]]:
+    def get_weather(loc: str) -> Dict[str, Union[str, Any]]:
         """this function basically will do everything for this backend
 
         :param loc: either city/town name or zip code
@@ -80,6 +95,18 @@ def main() -> None:
         coord_data = get_coordinates(loc)
 
         forecast = get_forecast(coord_data["lat"], coord_data["lon"])
+        return jsonify({"location": coord_data, "forecast": forecast})
+
+    @server.route("/coordinates")
+    def get_weather_with_coords() -> Dict[str, Union[str, Any]]:
+        """get weather with coords directly input"""
+
+        lat = request.args.get("lat")
+        lon = request.args.get("lon")
+
+        # TODO: error handling for None return of lat and lon
+        coord_data = get_reverse_geoloc(lat, lon)
+        forecast = get_forecast(lat, lon)
         return jsonify({"location": coord_data, "forecast": forecast})
 
     server.run(debug=args.debug, port=args.port)
